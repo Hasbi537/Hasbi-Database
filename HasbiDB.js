5a41041568e0c0862bfd1f7b5f72f0ab7a4b88db
Hasbi537 <toregion74@gmail.com>
2025-05-31 13:26:56 +0800

    Update HasbiDB.js

--- HasbiDB.js
+++ HasbiDB.js
@@ -1,7 +1,15 @@
 {
-   secretKey: "hasbianakbaik",
-   Access: [
-      "6281937188",
-      "6285184827"
-   ]
+  "creator": "6285787579304",
+  "secretCode": "Hasbianakbaik",
+  "description": "secretKey for Hasbi.",
+  "lastUpdated": "30-05-2025",
+  "author": "Hasbi",
+  "status": "Active",
+  "license": "MIT",
+  "version": "5.1.2",
+  "Access": [
+    "6285787579304",
+    "62",
+    "62"
+  ]
 }
